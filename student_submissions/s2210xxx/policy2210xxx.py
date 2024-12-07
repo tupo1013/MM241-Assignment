e9f8496ca7c9a6251b39a91b3ddc616855e9023b
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-12-07 03:41:02 -0800

    Allow multiple policies

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -2,9 +2,14 @@ from policy import Policy
 
 
 def Policy2210xxx(Policy):
-    def __init__(self):
+    def __init__(self, policy_id=1):
+        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
+
         # Student code here
-        pass
+        if policy_id == 1:
+            pass
+        elif policy_id == 2:
+            pass
 
     def get_action(self, observation, info):
         # Student code here
